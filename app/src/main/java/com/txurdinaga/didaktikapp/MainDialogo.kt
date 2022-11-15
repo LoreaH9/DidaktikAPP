0636007d975f0128367a246342ab0c111c869366
martiniuwu <115216787+martiniuwu@users.noreply.github.com>
2022-11-15 13:09:32 +0100

    Layout dialogos

--- app/src/main/java/com/txurdinaga/didaktikapp/MainDialogo.kt
+++ app/src/main/java/com/txurdinaga/didaktikapp/MainDialogo.kt
@@ -8,6 +8,6 @@ class MainDialogo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_dialogo)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     }
 }
